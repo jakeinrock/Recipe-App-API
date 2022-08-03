0fa554635071fe66da5504ec83d81f116896cf61
Jakub Skulski <j.j.skulski@gmail.com>
2022-08-03 10:09:54 +0200

    #16 Recipe Image API implemented

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -33,6 +33,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True, required=False)
     ingredients = IngredientSerializer(many=True, required=False)
 
+
     class Meta:
         model = Recipe
         fields = [
@@ -93,4 +94,14 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view."""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description', 'image']
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipes."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': 'True'}}
